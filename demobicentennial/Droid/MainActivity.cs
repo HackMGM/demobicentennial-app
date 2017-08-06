e5fbef922ce610c5ac6597e5a49fea09645f1f24
Elijah Lofgren <elijahlofgren@gmail.com>
2017-08-06 17:03:32 -0500

    WIP on #1 - Got map to show in iOS simulator.
    Following
    https://developer.xamarin.com/guides/xamarin-forms/user-interface/map/

--- demobicentennial/Droid/MainActivity.cs
+++ demobicentennial/Droid/MainActivity.cs
@@ -21,7 +21,7 @@ namespace demobicentennial.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-
+            Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
 
--- demobicentennial/demobicentennial/Views/ItemsPage.xaml.cs
+++ demobicentennial/demobicentennial/Views/ItemsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace demobicentennial
 {
@@ -15,6 +16,20 @@ namespace demobicentennial
             InitializeComponent();
 
             BindingContext = viewModel = new ItemsViewModel();
+
+            // Experiment from https://developer.xamarin.com/guides/xamarin-forms/user-interface/map/
+            var map = new Map(
+            MapSpan.FromCenterAndRadius(
+                    new Position(37, -122), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
         }
 
         async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
